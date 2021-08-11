7de1a63477ae5fba2f30f2fb43ac863195a7732f
anadege <ilmefautuneadresse@gmail.com>
2021-08-11 16:37:38 +0200

    unlock

--- philo/init_philo.c
+++ philo/init_philo.c
@@ -6,7 +6,7 @@
 /*   By: anadege <anadege@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 20:40:20 by anadege           #+#    #+#             */
-/*   Updated: 2021/08/11 12:01:22 by anadege          ###   ########.fr       */
+/*   Updated: 2021/08/11 16:34:03 by anadege          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,9 @@ void	*philo_launch(void *received)
 		usleep(10);
 		script_for_philo(philo, &take_right_fork, &take_left_fork);
 	}
+	pthread_mutex_unlock(&philo->is_eating);
+	pthread_mutex_unlock(&philo->args->forks[philo->left_fork]);
+	pthread_mutex_unlock(&philo->args->forks[philo->right_fork]);
 	return (NULL);
 }
 
@@ -42,7 +45,7 @@ int	init_threads(t_philo *philo, t_arguments *args)
 	}
 	while (1)
 	{
-		usleep(1000);
+		usleep(500);
 		if (check_nbr_of_meals(philo, args) == 1
 			|| check_if_dead(philo, args) == 1)
 		{
